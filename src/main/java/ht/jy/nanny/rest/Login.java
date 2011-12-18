ebf4213dd985b94bac50f670dbd0d700a06ac425
Jeremy Herault <jeremy.herault@gmail.com>
2011-12-18 23:09:45 +0100

    bad REST login mapping in the login form + few changes

--- src/main/java/ht/jy/nanny/rest/Login.java
+++ src/main/java/ht/jy/nanny/rest/Login.java
@@ -1,6 +1,7 @@
 package ht.jy.nanny.rest;
 
 import ht.jy.nanny.couchdb.CouchDB;
+import ht.jy.nanny.utils.Utils;
 import org.codehaus.jackson.JsonNode;
 
 import javax.ws.rs.FormParam;
@@ -17,6 +18,6 @@ public class Login {
     public String login(@FormParam("login") String login, @FormParam("pwd") String password){
 
         JsonNode nanny = CouchDB.getInstance().connect(login);
-        return nanny.get("password").getTextValue().equals(password) ? "" : "";
+        return nanny.get("password").getTextValue().equals(password) ? Utils.NANNY_REDIRECT : "BAD BAD BAD I'M BAD...";
     }
 }
