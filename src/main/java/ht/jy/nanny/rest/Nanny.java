12b8505e216d5f11e93f3cdab51831c8a67f32ae
Jeremy Herault <jeremy.herault@gmail.com>
2012-01-20 13:43:34 +0100

    test

--- src/main/java/ht/jy/nanny/rest/Nanny.java
+++ src/main/java/ht/jy/nanny/rest/Nanny.java
@@ -1,6 +1,8 @@
 package ht.jy.nanny.rest;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 /**
  *  @author Jeremy Herault
@@ -9,5 +11,10 @@ import javax.ws.rs.Path;
 @Path("nanny")
 public class Nanny {
 
-
+       public Response update(@QueryParam("nanny") String nanny){
+           
+          Response response = Response.ok().build();
+           
+          return response;
+       } 
 }
